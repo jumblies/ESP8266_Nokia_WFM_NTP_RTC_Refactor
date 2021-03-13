9caacac659eca1ce65ef878c201f7ebe15d0f46a
glamke <glamke@gmail.com>
2021-03-13 05:45:19 -0500

    split out on demand wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+/* 
+REFACTOR 2021-03
+  setup_wifi_config.h employs WiFiManager only when pin 10 is pulled low.  
+    File contains code normally in startup
+  
+
+*/
 #include <Arduino.h>
 
 // Date and time functions using a DS3231 RTC connected via I2C and Wire lib
@@ -25,10 +32,10 @@ time_t utc, local;
 // u8g2 Constructor
 #include <U8g2lib.h>
 U8G2_PCD8544_84X48_1_4W_SW_SPI u8g2(U8G2_R0,
-  /* clock=*/D3, /* data=*/D7, /* cs=*/D8, /* dc=*/D6, /* reset=*/D0);
+                                    /* clock=*/D3, /* data=*/D7, /* cs=*/D8, /* dc=*/D6, /* reset=*/D0);
 
 #include "clock_draw.h"
-
+#include "setup_wifi_config.h"
 
 unsigned long previousMillis;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
@@ -36,9 +43,9 @@ char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 void setup()
 {
 
-  delay(500); //added to prevent current spike reboots
   Serial.begin(115200);
-    while (!Serial) {
+  while (!Serial)
+  {
     delay(100); // wait for serial port to connect. Needed for Native USB only
   }
 
@@ -51,33 +58,15 @@ void setup()
 
   // if (rtc.lostPower())
   // {
-    // Serial.println("RTC lost power, let's set the time!");
-    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-      // }
-
+  // Serial.println("RTC lost power, let's set the time!");
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // }
 
   u8g2.begin();
-  u8g2.firstPage();
-  do
+  pinMode(10, INPUT);
+  if (digitalRead(10) == LOW)
   {
-    u8g2.setContrast(120);
-    u8g2.setFontMode(1);  /* activate transparent font mode */
-    u8g2.setDrawColor(1); /* color 1 for the box */
-    u8g2.drawBox(0, 0, 84, 48);
-    u8g2.setFont(u8g2_font_ncenB14_tr); /* select Font */
-    u8g2.setDrawColor(2);
-    u8g2.drawStr(18, 20, "WiFi");
-    u8g2.drawStr(12, 42, "Check");
-  } while (u8g2.nextPage());
-  delay(50);
-
-  WiFiManager wifiManager;
-  wifiManager.resetSettings();
-  wifiManager.setTimeout(5);
-  wifiManager.autoConnect("NokiaClock");
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    Serial.printf("Wifi Connected; IP = %s\n", WiFi.localIP().toString().c_str());
+    setup_wifi();
   }
   else
   {
@@ -95,11 +84,12 @@ void loop()
   if (currentMillis - previousMillis >= 1000)
   { //update the display only if time has changed
     previousMillis = currentMillis;
-    Serial.print("Temperature: ");
+
     float fahrenheit = ((rtc.getTemperature() * 9 / 5) + 32); //Convert Temp to Fahrenheit
-    Serial.print(fahrenheit);
-    Serial.printf("\t %02d %02d %04d\n", now.month(), now.day(), now.year());
-    Serial.println();
+    // Serial.print("Temperature: ");
+    // Serial.print(fahrenheit);
+    // Serial.printf("\t %02d %02d %04d\n", now.month(), now.day(), now.year());
+    // Serial.println();
     digitalClockDisplay(now, fahrenheit);
   }
 }
---
+++ src/setup_wifi_config.h
@@ -0,0 +1,24 @@
+void setup_wifi()
+{
+    u8g2.firstPage();
+    do
+    {
+        u8g2.setContrast(120);
+        u8g2.setFontMode(1);  /* activate transparent font mode */
+        u8g2.setDrawColor(1); /* color 1 for the box */
+        u8g2.drawBox(0, 0, 84, 48);
+        u8g2.setFont(u8g2_font_ncenB14_tr); /* select Font */
+        u8g2.setDrawColor(2);
+        u8g2.drawStr(18, 20, "WiFi");
+        u8g2.drawStr(12, 42, "Check");
+    } while (u8g2.nextPage());
+    delay(50);
+
+    WiFiManager wifiManager;
+    wifiManager.setTimeout(15);
+    wifiManager.startConfigPortal("NokiaClockAP");
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        Serial.printf("Wifi Connected; IP = %s\n", WiFi.localIP().toString().c_str());
+    }
+}
